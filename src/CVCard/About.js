f83dec27712d1f3e7e5c75c3de045f2c621d37d6
Serge044 <serhiima@wix.com>
2023-03-28 14:52:59 +0300

    removed unnessesary stuff

---
+++ src/CVCard/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="divAbout">
+      <h3>About</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam provident
+        praesentium perferendis, porro similique cupiditate, nobis cum
+        voluptates tempora explicabo a omnis? Voluptatum voluptatibus atque
+        voluptates iusto sapiente cumque dolor?
+      </p>
+    </div>
+  );
+}
